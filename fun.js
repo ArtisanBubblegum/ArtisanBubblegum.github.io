2b47edb4632714c2fd0f4128a942958005a8c06e
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2021-11-16 22:57:00 -0800

    Update fun.js

--- fun.js
+++ fun.js
@@ -17,5 +17,5 @@ function myFunction()
 
 function changeBGColor() 
 {
-  document.getElementById("BodyStyle").style.backgroundColor = "purple";
+  document.getElementById("body1").style.backgroundColor = "purple";
 }
