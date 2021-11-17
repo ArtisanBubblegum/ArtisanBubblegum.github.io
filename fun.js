a14508ad87ab1c6807d8c8c2b57a7a287c0768ce
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2021-11-16 22:38:08 -0800

    Update fun.js

--- fun.js
+++ fun.js
@@ -7,7 +7,7 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "No, why? Why did you click that button?";
     i += 1;
   }
-  if (i == 1)
+  else if (i == 1)
   {
     document.getElementById("demo").innerHTML = "Word Famous Artisan";
     i += -1;
