1c20dac211bbe55b5de24d8da7f93e6f31af65da
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2021-11-16 22:29:58 -0800

    Update fun.js

--- fun.js
+++ fun.js
@@ -2,6 +2,6 @@
 
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+  document.getElementById("demo").innerHTML = "No, why? Why did you click that button?";
   //document.setAttribute("demo", "set new value to para");
 }
