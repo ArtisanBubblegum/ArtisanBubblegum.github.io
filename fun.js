14382623ecd42b49c800351385d1a7a71c750c0e
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2021-11-16 22:40:12 -0800

    Update fun.js

--- fun.js
+++ fun.js
@@ -4,12 +4,12 @@ var i = 0;
 function myFunction() {
   if (i == 0)
   {
-    document.getElementById("demo").innerHTML = "No, why? Why did you click that button?";
+    document.getElementById("header1").innerHTML = "No, why? Why did you click that button?";
     i += 1;
   }
   else if (i == 1)
   {
-    document.getElementById("demo").innerHTML = "Word Famous Artisan";
+    document.getElementById("header1").innerHTML = "Word Famous Artisan";
     i += -1;
   }
 }
