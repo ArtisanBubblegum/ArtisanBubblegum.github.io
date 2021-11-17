e030e75228a5bb3f224297176aa844ea22a7e2ec
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2021-11-16 23:18:15 -0800

    Update fun.js

--- fun.js
+++ fun.js
@@ -5,13 +5,23 @@ function myFunction()
 {
   if (i == 0)
   {
-    document.getElementById("header1").innerHTML = "No, why? Why did you click that button?";
+    document.getElementById("header1").innerHTML = "No, why? Why did you click there?";
     i += 1;
   }
   else if (i == 1)
   {
     document.getElementById("header1").innerHTML = "Word Famous Artisan";
-    i += -1;
+    i += 1;
+  }
+  else if (i == 2)
+  {
+    document.getElementById("header1").innerHTML = "Oof!";
+    i += 1;
+  }
+  else if (i == 3)
+  {
+    document.getElementById("header1").innerHTML = "Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot! Riot!";
+    i = 0;
   }
 }
 
