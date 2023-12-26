d3b24f8c23c8bae204e068929aa51c33aabdc3d1
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2023-12-26 03:06:05 -0800

    Add files via upload

---
+++ StartHere.js
@@ -0,0 +1,118 @@
+let Player = {
+    "Name" : "player one",
+    "HP" : 100,
+    "Atk" : 7,
+    "Battle Stats" : {"ATK": 8, "DEF": 14, "MP" : 25},
+    "Position" : [3,3],
+}
+
+function mapMaker(x, y){
+    x = x * 1;
+    y = y * 1;
+    if (x < 5) { x = 5;}
+    if (x > 65) {x = 65;}
+    if (y < 5) { y = 5;}
+    if (y > 50) {y = 50;}
+    let map = [];
+    map.push([x,y]);
+    mapSize = x*y;
+    curIndex = 1;
+    while (curIndex <= mapSize){
+        if (curIndex <= x || curIndex % x == 0 || (curIndex-1) % x == 0 || curIndex > mapSize-x){
+            map.push([1,0]);
+        }
+        else{
+            map.push([0,0]);
+        }
+        curIndex++;
+    }
+    return map;
+}
+
+let Map1 = mapMaker (7,7); //mapMaker(prompt("Horizontal Size of Map: ", 10),prompt("Vertical Size of Map: ", 10));
+
+let Map2 = [[5,5],
+    [1,0], [1,0], [1,0], [1,0], [1,0],
+    [1,0], [0,0], [0,0], [0,0], [1,0],
+    [1,0], [0,0], [0,0], [0,0], [1,0],
+    [1,0], [0,0], [0,0], [0,0], [1,0],
+    [1,0], [1,0], [1,0], [1,0], [1,0],
+];
+
+function DrawMap (playerLocation, map){
+    let mapText = "";
+    let x = 1;
+    let playerIndex = playerLocation[1]*map[0][0];
+    playerIndex -= map[0][0];
+    playerIndex += playerLocation[0];
+    while (x < map.length){
+        mapText += "["
+        if ( x == playerIndex){
+            mapText += "P, -";
+        }
+        else {
+            mapText += GetMapID([x, 1], Map1);
+        }
+        mapText += "] "
+        if (x % map[0][0] == 0){
+            mapText += "\r\n";
+            //mapText += "\n";
+        }
+        x++;
+    }
+    document.getElementById("MapCanvas").textContent = mapText;
+    return mapText;
+}
+
+function GetMapID (location, map){
+    let mapIndex = location[1]*map[0][0];
+    mapIndex -= map[0][0];
+    mapIndex += location[0];
+    
+    if (mapIndex>0 && mapIndex<map.length){
+        return Map1[mapIndex];
+    }
+    else {
+        console.log("Invalid Map Index: ", mapIndex);
+        return false;
+    }
+}
+
+//before start:
+DrawMap(Player.Position, Map1);
+
+//update:
+function stepFunction(input){
+    if (typeof input == "string"){
+        alert("Invalid Input Type: String.")
+        //This will eventually be used for other inputes.
+    }
+    else if (typeof input == "object"){
+        if (typeof input[0] == "number"){
+            //Direction
+            TryToMove(input);
+        }
+        else {
+            alert("Invalid Array Type: Other.")
+        }
+    }
+    else {
+        alert("Invalid Input Type: Other.")
+    }
+
+    DrawMap(Player.Position, Map1);
+}
+
+//sub-functions of update:
+function TryToMove(dir){
+    switch(GetMapID([Player.Position[0] + dir[0], Player.Position[1] + dir[1]], Map1)[0]){
+        case 0:
+            Player.Position[0] += dir[0];
+            Player.Position[1] += dir[1];
+            console.log("Moved to ", Player.Position);
+            break;
+        default:
+            console.log("Stopped at ", Player.Position);
+            break;
+    }
+}
