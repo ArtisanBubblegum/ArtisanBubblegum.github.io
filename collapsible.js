0c376cecee3bb945277ddacea15c76a6593593c5
ArtisanBubblegum <94413397+ArtisanBubblegum@users.noreply.github.com>
2025-06-07 01:48:28 -0700

    Add files via upload
    Adding Self Collapse funtion

--- collapsible.js
+++ collapsible.js
@@ -1,4 +1,5 @@
 var coll = document.getElementsByClassName("collapsible");
+var collUP = document.getElementsByClassName("collapsibleUP")
 var i;
 
 for (i = 0; i < coll.length; i++) {
@@ -11,6 +12,20 @@ for (i = 0; i < coll.length; i++) {
     } else {
       content.style.display = "block";
       this.textContent = "Collapse"
+    }
+  });
+}
+
+for (i = 0; i < collUP.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.parentElement;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      this.parentElement.previousElementSibling.textContent = "Expand"
+    } else {
+      content.style.display = "block";
+      this.parentElement.previousElementSibling.textContent = "Collapse"
     }
   });
 }
